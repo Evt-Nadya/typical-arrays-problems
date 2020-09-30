8e77052b3d9b5f3d4ec7ca3b7726a2214b8bd04c
Evt-Nadya <49037722+Evt-Nadya@users.noreply.github.com>
2020-09-30 23:06:48 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    var len = array.length;
+    if (len === 0) {
+        return 0;
+    }
+    var min = array[0];
+    for (var i = 0; i < len; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    var len = array.length;
+    if (len === 0) {
+        return 0;
+    }
+    var max = array[0];
+    for (var i = 0; i < len; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    var len = array.length;
+    if (len === 0) {
+        return 0;
+    }
+    var sum = 0;
+    for (var i = 0; i < len; i++) {
+        sum = sum + array[i];
+    }
+    return sum / len;
 }
